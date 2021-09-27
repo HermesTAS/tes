9ee935c53a947fb300107d49c59327f942d9493c
HermesTAS <hermestaspusat@gmail.com>
2021-09-27 12:20:28 +0700

    mergin

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-// fwagfawg
-// dwafwalih
-// update from remote_branch
-// remote comment
-// new repo comment
-=======
->>>>>>> random_remote_branch
 echo 'tes123';
 // working on new branch
 // repo comment
